26a809120062b190c0dfd5fb60bcf3778d7fab49
Iordanis Kostelidis <ikostelidis@jnksoftware.eu>
2020-04-27 15:07:14 +0300

    Optimize test

--- src/test/java/eu/jnksoftware/europass/sdk/service/EuropassConvertTest.java
+++ src/test/java/eu/jnksoftware/europass/sdk/service/EuropassConvertTest.java
@@ -5,9 +5,11 @@ import eu.jnksoftware.europass.sdk.utils.FileUtils;
 import io.vavr.control.Option;
 import io.vavr.control.Try;
 import org.apache.commons.io.IOUtils;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
 
 public class EuropassConvertTest {
@@ -21,7 +23,7 @@ public class EuropassConvertTest {
 
     @Test
     public void testXmlToJson_Success() {
-        Try.of(this::getClass)
+        Try<File> files = Try.of(this::getClass)
                 .map(Class::getClassLoader)
                 .flatMap(classLoader -> Try.of(() -> classLoader.getResource("europass-cv-example-v3.4.0.xml")))
                 .flatMap(url -> Option.of(url.getFile()).toTry())
@@ -29,9 +31,8 @@ public class EuropassConvertTest {
                 .flatMap(file -> europassConvert.convert(
                         file,
                         Conversion.TO_JSON
-                ))
-                .flatMap(FileUtils::constructInputStream)
-                .flatMap(inputStream -> Try.of(() -> IOUtils.toString(inputStream, StandardCharsets.UTF_8)))
-                .peek(x -> System.out.println(x));
+                ));
+
+        Assert.assertTrue(files.isSuccess());
     }
 }
